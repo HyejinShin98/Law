cc052f37597b58c64f0f6feda3d136a3be19ad78
7090848 <7090848@naver.com>
2022-09-22 15:18:47 +0900

    인터셉터 적용

---
+++ Law/src/main/java/com/br/law/interceptor/SessionInterceptor.java
@@ -0,0 +1,31 @@
+package com.br.law.interceptor;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class SessionInterceptor implements HandlerInterceptor{
+	
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception /*ModelAndViewDefiningException*/{
+		
+		System.out.println("인터셉터 확인용");
+		if(request.getSession().getAttribute("user")==null) {
+//			ModelAndView mv = new ModelAndView();
+//			mv.setViewName("user/main");//이게 null일 경우 이페이지로 이동
+//			throw new ModelAndViewDefiningException(mv);
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>alert('로그인이 필요합니다 !'); location.href='../../law/user/main'; </script> ");
+			out.flush();
+			out.close();
+			
+			return false;
+		}
+		return true;
+	}
+	
+}
