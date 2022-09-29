536f98cbb6eb72bd01fb32e281aa8f942c8b4fd1
HyejinShin <106369817+HyejinShin98@users.noreply.github.com>
2022-09-29 20:51:11 +0900

    [hyejin] 나의 등재신청 수정페이지 js 및 로직 수정;

--- Law/src/main/java/com/br/law/controller/user/TrialUserRestController.java
+++ Law/src/main/java/com/br/law/controller/user/TrialUserRestController.java
@@ -1,6 +1,10 @@
 package com.br.law.controller.user;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -8,7 +12,10 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.br.law.service.user.TrialUserService;
 import com.br.law.vo.Tb_001;
@@ -229,24 +236,98 @@ public class TrialUserRestController {
 	}
 	
 	@RequestMapping("modifyTableNine")
-	public Map<String, Object> modifyTableNine(Tb_009 param) {
+//	public Map<String, Object> modifyTableNine(MultipartHttpServletRequest request, HttpSession session) {
+	public Map<String, Object> modifyTableNine(@RequestParam HashMap<Object, Object> param, MultipartHttpServletRequest request, HttpSession session) {
 		Map<String, Object> map = new HashMap<String, Object>();
+		Tb_001 sessionUser = (Tb_001)session.getAttribute("user");
+		int aplcn_dtls_proper_num =  Integer.parseInt(request.getParameter("aplcn_dtls_proper_num"));
 		
 		try {
-			if(trialMainService.modifyTableNine(param) > 0) {
-				map.put("result", "success");
-				map.put("msg", "저장되었습니다");
-				LOGGER.info("trialMainServie modifyTableNine Success! Tb_009 : " + param.toString());
-			} else {
-				map.put("result", "fail");
-				map.put("msg", "저장에 실패하였습니다.");
+			
+			
+			Iterator<String> iter = request.getFileNames();
+			MultipartFile mfile = null;
+			String fileName = "";
+			while(iter.hasNext()) {
+				fileName = (String) iter.next();	// 파일이름
+				mfile = request.getFile(fileName);
 			}
+			
+			
+			List<MultipartFile> fileList = request.getFiles("files");
+			List<Tb_009> list = new ArrayList<Tb_009>();
+			
+			String[] codes = request.getParameterValues("codes");
+					
+			String[] types = request.getParameterValues("types");
+			
+			System.out.println("codes : " + Arrays.toString(codes));
+			System.out.println("types : " + Arrays.toString(types));
+			
+			/*
+			int i=0;
+			for(MultipartFile file : fileList) {
+				String path = "C:uploadFiles/";
+				String originFileName = file.getOriginalFilename();
+				String nameFile = System.currentTimeMillis() + originFileName;
+				File userFolder = new File(path + userNo + userName);
+				if(!userFolder.exists()) {
+					userFolder.mkdirs();
+				}
+				
+				// 개별 insert
+				file.transferTo(new File(path + userNo + userName + "/" + nameFile));
+				Tb_009 atch = new Tb_009();
+				atch.setOriginal_file_name(originFileName);
+				atch.setFile_path(path);
+				atch.setFile_code(codes[i]);
+				atch.setFile_type(types[i]);
+				atch.setAplcn_dtls_proper_num(aplcn_dtls_proper_num);
+				
+				list.add(atch);
+				i++;
+			}
+			*/
+			
+			trialMainService.modifyTableNine(list);
+			map.put("result", "success");
+			map.put("msg", "저장되었습니다");
+			LOGGER.info("trialMainService modifyTableNine SUCCESS! list : " + list);
+			
 		} catch (Exception e) {
 			map.put("result", "error");
 			map.put("msg", e.getMessage());
-			LOGGER.error("trialMainService modifyTableNine ERROR! aplcn_dtls_proper_num : " + param.getAplcn_dtls_proper_num());
+			LOGGER.error("trialMainService modifyTableNine ERROR! aplcn_dtls_proper_num : " + aplcn_dtls_proper_num);
 			e.printStackTrace();
 		}
+		return map;
+	}
+	
+	@RequestMapping("updateAplcnStsToExamination")
+	public Map<String, Object> updateAplcnStsToExamination(HttpSession session, String aplcn_dtls_proper_num) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			int result = trialMainService.updateApplicationStatus(Integer.parseInt(aplcn_dtls_proper_num), "examination");
+			
+			if(result > 0) {
+				
+				LOGGER.info("trialMainService updateAplcnStsToExamination SUCCESS! aplcn_dtls_proper_num : " + aplcn_dtls_proper_num);
+				map.put("result", "success");
+				map.put("msg", "수정완료 되었습니다.");
+			} else {
+				
+				LOGGER.error("trialMainService updateAplcnStsToExamination ERROR! aplcn_dtls_proper_num : " + aplcn_dtls_proper_num);
+				map.put("result", "fail");
+				map.put("msg", "수정실패하였습니다.");
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("result", "error");
+			map.put("msg", e.getMessage());
+		}
+		
 		return map;
 	}
 	
